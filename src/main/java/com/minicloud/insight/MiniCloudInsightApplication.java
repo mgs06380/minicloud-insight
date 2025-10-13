bd4cb4cafb5f85948bb6de19f77764332d727d35
mgs06380 <mgs06380@naver.com>
2025-10-13 20:11:12 +0900

    Initial commit: Spring Boot 프로젝트 생성

---
+++ src/main/java/com/minicloud/insight/MiniCloudInsightApplication.java
@@ -0,0 +1,13 @@
+package com.minicloud.insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCloudInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCloudInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minicloud/insight/MiniCloudInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minicloud.insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCloudInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
